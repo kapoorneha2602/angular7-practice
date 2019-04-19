6955c8f05619e23c2527e6f3f8c26033fa61d0d3
Neha Kapoor <neha@Nehas-MacBook-Air-2.local>
2019-04-19 19:15:54 +0530

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: '',pathMatch: 'full'},
+  { path: 'app-drag-drop', component: DragDropComponent },
+  { path: 'app-virtual-scroll', component: VirtualScrollComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropComponent,
+    VirtualScrollComponent
   ],
   imports: [
     BrowserModule,
