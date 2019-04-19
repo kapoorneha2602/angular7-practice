a4defb2eaf0c5c738ff322b703acf4953c4180b5
Neha Kapoor <neha@Nehas-MacBook-Air-2.local>
2019-04-19 23:42:12 +0530

    Add angular material and drag drop feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.component';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatMenuModule, MatInputModule, MatTableModule, MatButtonModule, MatCardModule, MatTableDataSource, MatPaginatorModule, MatSortModule } from '@angular/material'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,10 @@ import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule, MatMenuModule, MatInputModule, MatTableModule, MatButtonModule, MatCardModule, MatPaginatorModule, MatSortModule,  
+    DragDropModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
+//import {CdkDragDrop, moveItemInArray, transferArrayItem} from ‘@angular/cdk/drag-drop’;
+import { CdkDragDrop, moveItemInArray,transferArrayItem } from '@angular/cdk/drag-drop';  
 @Component({
   selector: 'app-drag-drop',
   templateUrl: './drag-drop.component.html',
   styleUrls: ['./drag-drop.component.scss']
 })
 export class DragDropComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
-
+  todos = [
+    {
+      name: 'Angular',
+      category: 'Web Development'
+    },
+    {
+      name: 'Flexbox',
+      category: 'Web Development'
+    },
+    {
+      name: 'iOS',
+      category: 'App Development'
+    },
+    {
+      name: 'Java',
+      category: 'Software development'
+    }
+  ];
+  
+  completed = [
+    {
+      name: 'Android',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'MongoDB',
+      category: 'Databases'
+    },
+    {
+      name: 'ARKit',
+      category: 'Augmented Reality'
+    },
+    {
+      name: 'React',
+      category: 'Web Development'
+    }
+  ];
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex, event.currentIndex);
+    }
+  }
 }
